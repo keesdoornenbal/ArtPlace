ecce6fadf03674c8f5ae6b8e95bb20ce9cfcfedc
keesdoornenbal <keesdoornenbal@gmail.com>
2019-03-08 17:24:50 +0100

    New index page, no login and register page
    The login and register action will now happen on the index page. There are no login and index pages anymore

---
+++ index.py
@@ -0,0 +1,149 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from artplace.auth import login_required
+from artplace.db import get_db
+
+bp = Blueprint('overview', __name__)
+
+@bp.route('/', methods=('GET', 'POST'))
+@bp.route('/index', methods=('GET', 'POST'))
+def index():
+    # db = get_db()
+    # posts = db.execute(
+    #     'SELECT p.id, title, body, created, author_id, username'
+    #     ' FROM post p JOIN user u ON p.author_id = u.id'
+    #     ' ORDER BY created DESC'
+    # ).fetchall()
+    if request.method == 'POST':
+        if request.form.post[action] == 'Login':
+            username = request.form['login_username']
+            password = request.form['login_password']
+            db = get_db()
+            error = None
+            user = db.execute(
+                'SELECT * FROM user WHERE username = ?', (username,)
+            ).fetchone()
+
+            if user is None:
+                error = 'Incorrect username.'
+            elif not check_password_hash(user['password'], password):
+                error = 'Incorrect password.'
+
+            if error is None:
+                session.clear()
+                session['user_id'] = user['id']
+                return redirect(url_for('index'))
+
+            flash(error)
+
+        elif request.form.post[action] == 'Register':
+            username = request.form['username']
+            password = request.form['password']
+            db = get_db()
+            error = None
+
+            if not username:
+                error = 'Username is required.'
+            elif not password:
+                error = 'Password is required.'
+            elif db.execute(
+                'SELECT id FROM user WHERE username = ?', (username,)
+            ).fetchone() is not None:
+                error = 'User {} is already registered.'.format(username)
+
+            if error is None:
+                db.execute(
+                    'INSERT INTO user (username, password) VALUES (?, ?)',
+                    (username, generate_password_hash(password))
+                )
+                db.commit()
+                session.clear()
+                session['user_id'] = user['id']
+                return redirect(url_for('index'))
+
+            flash(error)
+
+    return render_template('homepage/index.html')
+
+
+# @bp.route('/create', methods=('GET', 'POST'))
+# @login_required
+# def create():
+#     if request.method == 'POST':
+#         title = request.form['title']
+#         body = request.form['body']
+#         error = None
+#
+#         if not title:
+#             error = 'Title is required.'
+#
+#         if error is not None:
+#             flash(error)
+#         else:
+#             db = get_db()
+#             db.execute(
+#                 'INSERT INTO post (title, body, author_id)'
+#                 ' VALUES (?, ?, ?)',
+#                 (title, body, g.user['id'])
+#             )
+#             db.commit()
+#             return redirect(url_for('index'))
+#
+#     return render_template('overview/create.html')
+
+
+# def get_post(id, check_author=True):
+#     post = get_db().execute(
+#         'SELECT p.id, title, body, created, author_id, username'
+#         ' FROM post p JOIN user u ON p.author_id = u.id'
+#         ' WHERE p.id = ?',
+#         (id,)
+#     ).fetchone()
+#
+#     if post is None:
+#         abort(404, "Post id {0} doesn't exist.".format(id))
+#
+#     if check_author and post['author_id'] != g.user['id']:
+#         abort(403)
+#
+#     return post
+#
+# @bp.route('/<int:id>/update', methods=('GET', 'POST'))
+# @login_required
+# def update(id):
+#     post = get_post(id)
+#
+#     if request.method == 'POST':
+#         title = request.form['title']
+#         body = request.form['body']
+#         error = None
+#
+#         if not title:
+#             error = 'Title is required.'
+#
+#         if error is not None:
+#             flash(error)
+#         else:
+#             db = get_db()
+#             db.execute(
+#                 'UPDATE post SET title = ?, body = ?'
+#                 ' WHERE id = ?',
+#                 (title, body, id)
+#             )
+#             db.commit()
+#             return redirect(url_for('overview.index'))
+#
+#     return render_template('overview/update.html', post=post)
+#
+#
+# @bp.route('/<int:id>/delete', methods=('POST',))
+# @login_required
+# def delete(id):
+#     get_post(id)
+#     db = get_db()
+#     db.execute('DELETE FROM post WHERE id = ?', (id,))
+#     db.commit()
+#     return redirect(url_for('overview.index'))
